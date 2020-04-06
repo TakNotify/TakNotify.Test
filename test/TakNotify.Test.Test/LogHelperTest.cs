50670e2643cfab120652fd8adbb90371a2205b26
Frandi Dwi <frandi.dwijayanto@polyrific.com>
2020-04-06 09:54:55 +0700

    Init commit

---
+++ test/TakNotify.Test.Test/LogHelperTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TakNotify.Test.Test
+{
+    public class LogHelperTest
+    {
+        [Fact]
+        public void VerifyLog_Success()
+        {
+            // TODO: Implement test
+        }
+    }
+}
